b97f0d633137ea01a3c53d977deb34199f32d5c3
mikera <mike.r.anderson.13@gmail.com>
2013-04-12 20:11:19 +0800

    sum function

--- src/main/clojure/clojure/core/matrix/stats.clj
+++ src/main/clojure/clojure/core/matrix/stats.clj
@@ -1,9 +1,15 @@
 (ns clojure.core.matrix.stats
   (:use clojure.core.matrix))
 
+(defn sum 
+  "Calculates the sum of a collection of values. Values may be scalars, vectors or higher-dimensional matrices."
+  ([values]
+    (reduce add values)))
+
 (defn mean
   "Calculates the mean of a collection of values. Values may be scalars, vectors or higher-dimensional matrices."
   ([values]
     (let [n (count values)
-          s (reduce add values)]
+          s (sum values)]
       (mul s (/ 1.0 n)))))
+
--- src/test/clojure/clojure/core/matrix/test_stats.clj
+++ src/test/clojure/clojure/core/matrix/test_stats.clj
@@ -7,3 +7,6 @@
   (is (equals [1 2] (mean [[0 0] [1 2] [2 4]])))
   (is (equals 1.5 (mean [0 1 2 3]))))
 
+(deftest test-sum
+  (is (== 6 (sum [1 2 3 ])))
+  (is (equals [3 4] (sum [[1 1] [1 1] [1 2]])))) 
