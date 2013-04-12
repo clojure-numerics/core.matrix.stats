d2b58825a57b5733a0b87a560fc432ae274d7435
mikera <mike.r.anderson.13@gmail.com>
2013-04-12 19:57:14 +0800

    add deps, add cljunit tests

---
+++ src/test/java/clojure/core/matrix/StatsTest.java
@@ -0,0 +1,12 @@
+package clojure.core.matrix;
+
+
+import mikera.cljunit.ClojureTest;
+
+public class StatsTest extends ClojureTest {
+
+  //	@Override
+	public String filter() {
+		return "clojure.core.matrix";
+	}
+}
