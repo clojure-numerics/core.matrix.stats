fe8397610a071733e2bd4abc7ca4487e04535a61
mikera <mike.r.anderson.13@gmail.com>
2013-07-22 17:29:38 +0100

    extra tests

--- src/test/clojure/clojure/core/matrix/test_stats.clj
+++ src/test/clojure/clojure/core/matrix/test_stats.clj
@@ -5,17 +5,22 @@
 
 (deftest test-mean
   (is (equals [1 2] (mean [[0 0] [1 2] [2 4]])))
-  (is (equals 1.5 (mean [0 1 2 3]))))
+  (is (== 1.5 (mean [0 1 2 3]))))
 
 (deftest test-sum
   (is (== 6 (first (slices [6]))))
   (is (nil? (next (slices [6]))))
   (is (== 6 (sum [6])))
   (is (== 6 (sum [1 2 3])))
-  (is (equals [3 4] (sum [[1 1] [1 1] [1 2]])))) 
+  (let [ss (sum [[1 1] [1 1] [1 2]])]
+    (is (equals [3 4] ss))
+    (is (mutable? ss)))) 
 
 (deftest test-sum-of-squares
-  (is (== 30 (sum-of-squares [1 2 3 4]))))
+  (is (== 30 (sum-of-squares [1 2 3 4])))
+  (let [ss (sum-of-squares [[0 1] [1 2] [2 3] [3 4]])]
+    (is (equals [14 30] ss))
+    (is (mutable? ss))))
 
 (deftest test-variance
   (is (== 0.0 (variance [1 1 1])))
