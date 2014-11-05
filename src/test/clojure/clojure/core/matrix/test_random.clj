0f68902ae25fede6eb7435e324bb7cfbf3092f1b
mikera <mike.r.anderson.13@gmail.com>
2014-11-05 19:30:04 +0800

    extra random sequence tests

--- src/test/clojure/clojure/core/matrix/test_random.clj
+++ src/test/clojure/clojure/core/matrix/test_random.clj
@@ -8,10 +8,13 @@
   (is (= [10] (shape (sample-uniform [10]))))
   (is (number? (scalar (sample-uniform [])))))
 
-(deftest test-randoms
+(deftest test-random-sequence
   (let [rs (randoms)]
-    (is (= (take 100 rs) (take 100 rs)))
+    (is (= (take 1000 rs) (take 1000 rs)))
     (is (<= 0 (reduce + (take 1000 rs)) 1000)))
   (let [rs (nnext (randoms))]
-    (is (= (take 100 rs) (take 100 rs)))
+    (is (= (take 1000 rs) (take 1000 rs)))
     (is (<= 0 (reduce + (take 1000 rs)) 1000)))) 
+
+(deftest test-random-sequence-seeds
+  (is (= (take 1000 (randoms 1337)) (take 1000 (randoms 1337))))) 
